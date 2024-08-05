5880f41790b4e8eca83670893515d8622a604ff7
Humberto BM Junior <64866505+jrhumberto@users.noreply.github.com>
2024-08-05 02:19:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,41 @@ import zipfile
 import os
 import shutil
 
+
+def upload_myfile(content, token, repo_name, username = 'jrhumberto'):
+    # Cria o repositório
+    url = 'https://api.github.com/user/repos'
+    headers = {'Authorization': f'token {token}'}
+    payload = {'name': repo_name}
+    response = requests.post(url, headers=headers, json=payload)
+
+    # Cria um blob para o arquivo
+    url = f'https://api.github.com/repos/{username}/{repo_name}/git/blobs'
+    headers = {'Authorization': f'token {token}'}
+    payload = {'content': file_content, 'encoding': 'utf-8'}
+    response = requests.post(url, headers=headers, json=payload)
+    blob_sha = response.json()['sha']
+
+    # Cria uma árvore com o blob
+    url = f'https://api.github.com/repos/{username}/{repo_name}/git/trees'
+    headers = {'Authorization': f'token {token}'}
+    payload = {'tree': [{'path': file_path, 'mode': '100644', 'type': 'blob', 'sha': blob_sha}]}
+    response = requests.post(url, headers=headers, json=payload)
+    tree_sha = response.json()['sha']
+
+    # Cria um commit
+    url = f'https://api.github.com/repos/{username}/{repo_name}/git/commits'
+    headers = {'Authorization': f'token {token}'}
+    payload = {'message': 'Seu commit message', 'tree': tree_sha}
+    response = requests.post(url, headers=headers, json=payload)
+    commit_sha = response.json()['sha']
+
+    # Atualiza a referência do branch
+    url = f'https://api.github.com/repos/{username}/{repo_name}/git/refs/heads/main'
+    headers = {'Authorization': f'token {token}'}
+    payload = {'sha': commit_sha}
+    response = requests.patch(url, headers=headers, json=payload)
+
 def save_uploadedfile(uploadedfile):
     with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
         f.write(uploadedfile.getbuffer())
@@ -25,9 +60,14 @@ if uploaded_file is not None:
     st.write("Arquivos e subpastas no arquivo zip:")
     for root, dirs, files in os.walk("tempDir"):
         for filename in files:
-            st.text(os.path.join(root, filename))
+            # st.text(os.path.join(root, filename))
+            content = filename.getvalue()
+            upload_myfile(content=content, token=chave, repo_name=repositorio)
+            st.text(f'{filename}')
+            
     shutil.rmtree('tempDir')
 #
 if st.button("upload"):
     st.write('Conseguimos')
     st.text('E agora?')
+#
